5f21804c467678ecbcc3ccb033fc1aad21641b7a
“Casta <casta_ung123@hotmail.com>
2023-03-07 16:27:11 -0500

    1_declaration and template

---
+++ boggle.py
@@ -0,0 +1,69 @@
+###############################################################################
+## Jeu Boggle
+## Voici une simulation python simplifiée du jeu "Boggle".
+## Le jeu consiste à trouver des mots jusqu`à un maximum de 10 mots. 
+## Le pointage est calculé selon la taille de ces mots.
+## Un mot est valide s'il est composé de minimum 3 lettres adjacentes sur une même ligne ou colonne
+###############################################################################
+## Auteur: Maude Farmer et Casta Ung
+## Copyright: 2023, Boggle
+## Version: #1
+## Date: 2023-03-26
+###############################################################################
+
+# Aucun import car le jeu est assez simple
+
+# Déclaration des variables globales, constantes
+
+# Déclaration des dés sous forme de dictionnaire (donnés dans l'énoncé)
+des={'de1':['E', 'T', 'U', 'K', 'N', 'O'],
+     'de2':['E', 'V', 'G', 'T', 'I', 'N'],
+     'de3':['D', 'E', 'C', 'A', 'M', 'P'],
+     'de4':['I', 'E', 'L', 'R', 'U', 'W'],
+     'de5':['E', 'H', 'I', 'F', 'S', 'E'],
+     'de6':['R', 'E', 'C', 'A', 'L', 'S'],
+     'de7':['E', 'N', 'T', 'D', 'O', 'S'],
+     'de8':['O', 'F', 'X', 'R', 'I', 'A'],
+     'de9':['N', 'A', 'V', 'E', 'D', 'Z'],
+     'de10':['E', 'I', 'O', 'A', 'T', 'A'],
+     'de11':['G', 'L', 'E', 'N', 'Y', 'U'],
+     'de12':['B', 'M', 'A', 'Q', 'J', 'O'],
+     'de13':['T', 'L', 'I', 'B', 'R', 'A'],
+     'de14':['S', 'P', 'U', 'L', 'T', 'E'],
+     'de15':['A', 'I', 'M', 'S', 'O', 'R'],
+     'de16':['E', 'N', 'H', 'R', 'I', 'S'],
+     'de17':['A', 'T', 'S', 'I', 'O', 'U'],
+     'de18':['W', 'I', 'R', 'E', 'B', 'C'],
+     'de19':['Q', 'D', 'A', 'H', 'A', 'U'],
+     'de20':['A', 'C', 'F', 'L', 'N', 'E'],
+     'de21':['P', 'R', 'S', 'T', 'U', 'G'],
+     'de22':['J', 'P', 'R', 'X', 'E', 'Z'],
+     'de23':['E', 'K', 'V', 'Y', 'B', 'E'],
+     'de24':['A', 'L', 'C', 'H', 'E', 'M'],
+     'de25':['E', 'D', 'U', 'F', 'H', 'K'],
+     }
+
+# Déclaration des fonctions internes et calculs 
+# avec commentaires détaillés nécessaires seulement (optionnel)
+
+
+# Déclaration du code principal et Affichage
+
+
+def jouer():
+    return
+    
+def generer_grille(taille):
+    return
+    
+def est_valide(grille, mot):
+    return
+
+def calcul_point(grille, mots):
+    return
+
+#################################################################################
+# Tests
+def test() :
+    return
+#################################################################################
